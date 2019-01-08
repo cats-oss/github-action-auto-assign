5fdf1ca19ad9b840c0f76834c896b013fd1b3a07
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-01-09 04:52:44 +0900

    Split scanBarFoo into standalone function from Scanner

--- src/parser/scanner.js
+++ src/parser/scanner.js
@@ -166,7 +166,7 @@ class Scanner {
             };
         }
 
-        const value = this._scan();
+        const value = scanString(this._sourceIter);
         if (value.type === TokenType.Eof) {
             this._destroy();
         }
@@ -177,169 +177,194 @@ class Scanner {
         };
     }
 
-    _scan() {
-        const sourceIter = this._sourceIter;
-
-        const { done, value: char, } = sourceIter.next();
-        if (done) {
-            const t = new Token(TokenType.Eof, null);
-            return t;
-        }
-
-        if (isWhiteSpace(char)) {
-            return this._scanWhiteSpace(char);
-        }
-
-        if (isOperatorFragment(char)) {
-            return this._scanOperator(char);
-        }
-
-        if (isSeparator(char)) {
-            return this._scanSeparator(char);
-        }
-
-        if (isReviewDirective(char)) {
-            return this._scanReviewDirective(char);
-        }
-
-        if (isIdCall(char)) {
-            return this._scanUsername();
-        }
-
-        return this._scanIdentifier(char);
-    }
-
-    _scanWhiteSpace(char) {
-        const sourceIter = this._sourceIter;
-        let buffer = char;
-        for (;;) {
-            const { done, value } = sourceIter.next();
-            if (done) {
-                break;
-            }
-
-            if (!isWhiteSpace(value)) {
-                sourceIter.back();
-                break;
-            }
-
-            buffer += value;
-        }
-
-        const t = new Token(TokenType.WhiteSpace, buffer);
-        return t;
-    }
-
-    _scanIdentifier(char) {
-        const sourceIter = this._sourceIter;
-        let buffer = char;
-        for (;;) {
-            const { done, value } = sourceIter.next();
-            if (done) {
-                break;
-            }
-
-            if (!isPartOfIdentifier(value)) {
-                this._sourceIter.back();
-                break;
-            }
-
-            buffer += value;
-        }
-
-        const t = new Token(TokenType.Identifier, buffer);
-        return t;
-    }
-
-    _scanOperator(char) {
-        const sourceIter = this._sourceIter;
-        let buffer = char;
-
-        const { done, value } = sourceIter.next();
-        if (done) {
-            const t = new Token(TokenType.Invalid, buffer);
-            return t;
-        }
-
-        if (!isOperatorFragment(value)) {
-            const t = new Token(TokenType.Invalid, buffer);
-            return t;
-        }
-
-        if (char !== value) {
-            sourceIter.back();
-            const t = new Token(TokenType.Invalid, buffer);
-            return t;
-        }
-
-        buffer += value;
-        const t = new Token(TokenType.Operator, buffer);
-        return t;
-    }
-
-    _scanSeparator(char) {
-        switch (char) {
-            case CHAR_LIST_SEPATOR:
-                return new Token(TokenType.ListSeparator, char);
-            default:
-                return new Token(TokenType.Separator, char);
-        }
-    }
-
-    _scanReviewDirective(char) {
-        const sourceIter = this._sourceIter;
-        let buffer = char;
-
-        const { done, value } = sourceIter.next();
-        if (done) {
-            const t = new Token(TokenType.ReviewDirective, char);
-            return t;
-        }
-
-        if (isReviewOperator(value)) {
-            buffer += value;
-            switch (value) {
-                case CHAR_REVIEW_OPERATOR_QUESTION:
-                    return new Token(TokenType.AssignReviewerDirective, buffer);
-                case CHAR_REVIEW_OPERATOR_PLUS:
-                    return new Token(TokenType.AcceptPullRequestDirective, buffer);
-                case CHAR_REVIEW_OPERATOR_EQUAL:
-                    return new Token(TokenType.AcceptPullRequestWithReviewerNameDirective, buffer);
-                case CHAR_REVIEW_OPERATOR_MINUS:
-                    return new Token(TokenType.RejectPullRequestDirective, buffer);
-                default:
-                    throw new RangeError(`unrachable with isReviewOperator: ${value}`);
-            }
-        }
-
-        sourceIter.back();
-        return this._scanIdentifier(char);
-    }
-
-    _scanUsername() {
-        const sourceIter = this._sourceIter;
-        let buffer = '';
-        for (;;) {
-            const { done, value } = sourceIter.next();
-            if (done) {
-                break;
-            }
-
-            if (!isPartOfIdentifier(value)) {
-                this._sourceIter.back();
-                break;
-            }
-
-            buffer += value;
-        }
-
-        const t = new Token(TokenType.UserName, buffer);
-        return t;
-    }
-
     [Symbol.iterator]() {
         return this;
     }
+}
+
+/**
+ *  @param {!BackableStringIterator} charIter
+ *  @returns    {!Token}
+ */
+function scanString(charIter) {
+    const { done, value: char, } = charIter.next();
+    if (done) {
+        const t = new Token(TokenType.Eof, null);
+        return t;
+    }
+
+    if (isWhiteSpace(char)) {
+        return scanWhiteSpace(charIter, char);
+    }
+
+    if (isOperatorFragment(char)) {
+        return scanOperator(charIter, char);
+    }
+
+    if (isSeparator(char)) {
+        return scanSeparator(char);
+    }
+
+    if (isReviewDirective(char)) {
+        return scanReviewDirective(charIter, char);
+    }
+
+    if (isIdCall(char)) {
+        return scanUsername(charIter);
+    }
+
+    return scanIdentifier(charIter, char);
+}
+
+/**
+ *  @param {!BackableStringIterator} charIter
+ *  @param  {string}  char
+ *  @returns    {!Token}
+ */
+function scanWhiteSpace(charIter, char) {
+    let buffer = char;
+    for (; ;) {
+        const { done, value } = charIter.next();
+        if (done) {
+            break;
+        }
+
+        if (!isWhiteSpace(value)) {
+            charIter.back();
+            break;
+        }
+
+        buffer += value;
+    }
+
+    const t = new Token(TokenType.WhiteSpace, buffer);
+    return t;
+}
+
+/**
+ *  @param {!BackableStringIterator} charIter
+ *  @param  {string}  char
+ *  @returns    {!Token}
+ */
+function scanIdentifier(charIter, char) {
+    let buffer = char;
+    for (; ;) {
+        const { done, value } = charIter.next();
+        if (done) {
+            break;
+        }
+
+        if (!isPartOfIdentifier(value)) {
+            charIter.back();
+            break;
+        }
+
+        buffer += value;
+    }
+
+    const t = new Token(TokenType.Identifier, buffer);
+    return t;
+}
+
+/**
+ *  @param {!BackableStringIterator} charIter
+ *  @param  {string}  char
+ *  @returns    {!Token}
+ */
+function scanOperator(charIter, char) {
+    let buffer = char;
+
+    const { done, value } = charIter.next();
+    if (done) {
+        const t = new Token(TokenType.Invalid, buffer);
+        return t;
+    }
+
+    if (!isOperatorFragment(value)) {
+        const t = new Token(TokenType.Invalid, buffer);
+        return t;
+    }
+
+    if (char !== value) {
+        charIter.back();
+        const t = new Token(TokenType.Invalid, buffer);
+        return t;
+    }
+
+    buffer += value;
+    const t = new Token(TokenType.Operator, buffer);
+    return t;
+}
+
+/**
+ *  @param {string} char
+ *  @returns    {!Token}
+ */
+function scanSeparator(char) {
+    switch (char) {
+        case CHAR_LIST_SEPATOR:
+            return new Token(TokenType.ListSeparator, char);
+        default:
+            return new Token(TokenType.Separator, char);
+    }
+}
+
+/**
+ *  @param {!BackableStringIterator} charIter
+ *  @param  {string}  char
+ *  @returns    {!Token}
+ */
+function scanReviewDirective(charIter, char) {
+    let buffer = char;
+
+    const { done, value } = charIter.next();
+    if (done) {
+        const t = new Token(TokenType.ReviewDirective, char);
+        return t;
+    }
+
+    if (isReviewOperator(value)) {
+        buffer += value;
+        switch (value) {
+            case CHAR_REVIEW_OPERATOR_QUESTION:
+                return new Token(TokenType.AssignReviewerDirective, buffer);
+            case CHAR_REVIEW_OPERATOR_PLUS:
+                return new Token(TokenType.AcceptPullRequestDirective, buffer);
+            case CHAR_REVIEW_OPERATOR_EQUAL:
+                return new Token(TokenType.AcceptPullRequestWithReviewerNameDirective, buffer);
+            case CHAR_REVIEW_OPERATOR_MINUS:
+                return new Token(TokenType.RejectPullRequestDirective, buffer);
+            default:
+                throw new RangeError(`unrachable with isReviewOperator: ${value}`);
+        }
+    }
+
+    charIter.back();
+    return scanIdentifier(charIter, char);
+}
+
+/**
+ *  @param {!BackableStringIterator} charIter
+ *  @returns    {!Token}
+ */
+function scanUsername(charIter) {
+    let buffer = '';
+    for (; ;) {
+        const { done, value } = charIter.next();
+        if (done) {
+            break;
+        }
+
+        if (!isPartOfIdentifier(value)) {
+            charIter.back();
+            break;
+        }
+
+        buffer += value;
+    }
+
+    const t = new Token(TokenType.UserName, buffer);
+    return t;
 }
 
 function* tokenizeHighLevel(string) {
