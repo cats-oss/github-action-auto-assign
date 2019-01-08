2cd5a476774596b3867089e82943f728732422d8
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-01-09 00:42:38 +0900

    Add many test cases

--- src/parser/__tests__/test_parser.js
+++ src/parser/__tests__/test_parser.js
@@ -19,7 +19,34 @@ function expected(type, target) {
     return new Command(type, target);
 }
 
-const TEST_CASE_LIST = [
+function withBotName(array) {
+    return array.map(({ input, expected }) => {
+        return {
+            input: 'bors ' + input,
+            expected,
+        };
+    });
+}
+
+function withPrefixLikeBotName(array) {
+    return array.map(({ input }) => {
+        return {
+            input: 'bors' + input,
+            expected: null,
+        };
+    });
+}
+
+function withMultipleBotName(array) {
+    return array.map(({ input }) => {
+        return {
+            input: 'bors1 bors2 ' + input,
+            expected: null,
+        };
+    });
+}
+
+const REQUEST_REVIEW_TEST_CASE_LIST = [
     testCase('r? @bar', expected(CommandType.AssignReviewer, ['bar'])),
     testCase('r? @bar @foo', expected(CommandType.AssignReviewer, ['bar', 'foo'])),
     testCase('r? @bar || @foo', expected(CommandType.AssignReviewer, ['bar', 'foo'])),
@@ -37,29 +64,65 @@ const TEST_CASE_LIST = [
     testCase('\n r? @bar ||', null),
     testCase('r ? @bar || @bar', null),
     testCase('r? |&&', null),
+    testCase('r? ||&', null),
+    testCase('r? ||&&', null),
     testCase('    r?    &&', null),
     testCase('r? && foo', null),
     testCase('r? foo &&', null),
     testCase('\n r? @bar &&', null),
+    testCase('r ?', null),
     testCase('r ? @bar && @bar', null),
+];
 
+const APPROVE_REVIEW_TEST_CASE_LIST = [
     testCase('r+', expected(CommandType.AcceptPullRequest, [])),
-    testCase('r+ @bar', expected(CommandType.AcceptPullRequest, ['bar'])),
-    testCase('r+ @bar @foo', expected(CommandType.AcceptPullRequest, ['bar', 'foo'])),
-    testCase('   r+ @bar \n @foo', expected(CommandType.AcceptPullRequest, ['bar'])),
-    testCase('    r+    ', expected(CommandType.AcceptPullRequest, [])),
-    testCase('r+ foo', expected(CommandType.AcceptPullRequest, [])),
+    // TODO: testCase('r+ @bar', null),
+    // TODO: testCase('r+ @bar @foo', null),
+    // TODO: testCase('   r+ @bar \n @foo', null),
+    // TODO: testCase('    r+    ', null),
+    // TODO: testCase('r+ foo', null),
     testCase('\n r+ @bar', null),
     testCase('r +', null),
+    // TODO: testCase('r=bar', expected(CommandType.AcceptPullRequest, null)),
+    // TODO: testCase('r=bar,foo', null),
+    // TODO: testCase('r=', null),
+    // TODO: testCase('r = bar', null),
+    // TODO: testCase('r= \n foo', null),
+];
 
+const REJECT_REVIEW_TEST_CASE_LIST = [
     testCase('r-', expected(CommandType.RejectPullRequest, [])),
-    testCase('r- @bar', expected(CommandType.RejectPullRequest, ['bar'])),
-    testCase('r- @bar @foo', expected(CommandType.RejectPullRequest, ['bar', 'foo'])),
-    testCase('   r-  @bar \n @foo', expected(CommandType.RejectPullRequest, ['bar'])),
-    testCase('    r-    ', expected(CommandType.RejectPullRequest, [])),
-    testCase('r- foo', expected(CommandType.RejectPullRequest, [])),
+    // TODO: testCase('r- @bar', null),
+    // TODO: testCase('r- @bar @foo', null),
+    // TODO: testCase('   r-  @bar \n @foo', null),
+    // TODO: testCase('    r-    ', null),
+    // TODO: testCase('r- foo', null),
     testCase('\n r- @bar', null),
     testCase('r -', null),
+];
+
+const TEST_CASE_LIST = [
+    ...REQUEST_REVIEW_TEST_CASE_LIST,
+    ...[
+    // TODO:    ...withBotName(REQUEST_REVIEW_TEST_CASE_LIST),
+    // TODO:    ...withPrefixLikeBotName(REQUEST_REVIEW_TEST_CASE_LIST),
+    // TODO:    ...withMultipleBotName(REQUEST_REVIEW_TEST_CASE_LIST),
+    ],
+
+    ...APPROVE_REVIEW_TEST_CASE_LIST,
+    ...[
+    // TODO:    ...withBotName(APPROVE_REVIEW_TEST_CASE_LIST),
+    // TODO:    ...withPrefixLikeBotName(APPROVE_REVIEW_TEST_CASE_LIST),
+    // TODO:    ...withMultipleBotName(APPROVE_REVIEW_TEST_CASE_LIST),
+    ],
+
+    ...REJECT_REVIEW_TEST_CASE_LIST,
+    ...[
+    // TODO:    ...withBotName(REJECT_REVIEW_TEST_CASE_LIST),
+    // TODO:    ...withPrefixLikeBotName(REJECT_REVIEW_TEST_CASE_LIST),
+    // TODO:    ...withMultipleBotName(REJECT_REVIEW_TEST_CASE_LIST),
+    ],
+
 ];
 
 for (const testCase of TEST_CASE_LIST) {
