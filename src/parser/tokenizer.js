9cc8cb47092674ae6cb67133c577a11df4b91db9
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-12-23 21:15:46 +0900

    Move string scanner to a dedicated file

--- src/parser/tokenizer.js
+++ src/parser/tokenizer.js
@@ -1,54 +1,7 @@
 'use strict';
 
-const assert = require('assert');
-
+const { BackableStringIterator } = require('./string_scanner');
 const { Token, TokenType } = require('./token');
-
-class BackableStringIterator {
-    /**
-     * @param {string} str
-     */
-    constructor(str) {
-        this._iter = str[Symbol.iterator]();
-        this._prevCache = null;
-        this._prev = null;
-    }
-
-    next() {
-        const prev = this._prev;
-        if (prev !== null) {
-            this._prev = null;
-            this._prevCache = prev;
-            return {
-                done: false,
-                value: prev,
-            };
-        }
-        assert.strictEqual(prev, null, 'this._prev must be null');
-
-        const { done, value, } = this._iter.next();
-        if (done) {
-            this._prevCache = null;
-        } else {
-            this._prevCache = value;
-        }
-
-        return {
-            done,
-            value,
-        };
-    }
-
-    back() {
-        assert.notStrictEqual(this._prevCache, null, 'this._prev must not be null');
-        this._prev = this._prevCache;
-        this._prevCache = null;
-    }
-
-    [Symbol.iterator]() {
-        return this;
-    }
-}
 
 /**
  *  @param {string} char
